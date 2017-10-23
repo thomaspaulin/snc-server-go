11687fe29d38e45d0d6cf2dd50ff372e63f0c3bc
Thomas Paulin <thomas@biomatters.com>
2017-10-23 13:09:38 +1300

    * First (unchecked and untested) attempt at writing DB logic and handler funcs

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"database/sql"
 	"github.com/thomaspaulin/snc-server/database"
+	"github.com/thomaspaulin/snc-server/models"
+	"encoding/json"
 )
 
 func index(w http.ResponseWriter, req *http.Request) {
@@ -18,9 +20,40 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello, world!\n")
 }
 
+//todo break it up to support match IDs (use gorilla mux https://github.com/gorilla/mux)
 func matches(w http.ResponseWriter, req *http.Request) {
-	if req.Method == "GET" {
-
+	switch req.Method {
+		case "GET":
+			matches, err := models.FetchMatches(database.DB)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			w.Header().Set("Content-Type", "application/json")
+			encoder := json.NewEncoder(w);
+			encodeErr := encoder.Encode(matches)
+			if encodeErr != nil {
+				http.Error(w, encodeErr.Error(), http.StatusInternalServerError)
+			}
+			w.WriteHeader(http.StatusOK)
+		case "POST":
+			// todo remove this error when the create and update methods have been saved
+			http.Error(w, "Not implemented", http.StatusNotImplemented)
+			decoder := json.NewDecoder(req.Body)
+			matches := make([]*models.Match, 0)
+			err := decoder.Decode(matches)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			defer req.Body.Close()
+			for _, m := range matches {
+				err := m.Save(database.DB)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+				}
+			}
+			w.WriteHeader(http.StatusOK)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
 
@@ -37,6 +70,7 @@ func main() {
 	database.DB, err = sql.Open("sqlite3", "./snc.db")
 	handle(err)
 
+	// todo handle match IDs
 	http.HandleFunc("/", index)
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/matches", matches)
--- models/match.go
+++ models/match.go
@@ -2,9 +2,11 @@ package models
 
 import (
 	"time"
-	"github.com/jinzhu/gorm"
+	"database/sql"
+	"errors"
 )
 
+// todo handle the errors properly
 const (
 	PracticeGame = "PR"
 	RegularGame = "RS"
@@ -18,7 +20,6 @@ const (
 // Models
 
 type Match struct {
-	gorm.Model
 	id			uint32
 	// Datetime of the match start in UTC
 	start 		time.Time
@@ -78,3 +79,71 @@ type Penalty struct {
 }
 
 // Database logic
+func (m *Match) Save(db *sql.DB) error {
+	err := m.create(db)
+	if err != nil {
+		return m.update(db)
+	} else {
+		return nil
+	}
+}
+
+func FetchMatches(db *sql.DB) ([]*Match, error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+	rows, err := tx.Query("SELECT * FROM matches")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	matches := make([]*Match, 0)
+	for rows.Next() {
+		m := new(Match)
+		err := rows.Scan(&m.id, &m.start, &m.season, &m.away, &m.home, &m.awayScore, &m.homeScore, &m.rink)
+		// err here is the row.Scan(...) error. It shadows the err from outside the loop, and does not overwrite
+		if err != nil {
+			// probably the schema is wrong or the row is bad and so the database needs inspecting
+			// later on this might want to be changed to pass through and list the IDs of the bad rows
+			return nil, err
+		}
+		matches = append(matches, m)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	err = tx.Commit()
+	if err != nil {
+		return nil, err
+	}
+	return matches, nil
+}
+
+func (m *Match) create(db *sql.DB) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+	stmt, err := tx.Prepare("INSERT INTO matches (start, season, away, home, awayScore, homeScore, rink) VALUES (?, ?, ?, ?, -1, -1, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(m.start, m.season, m.awayScore, m.home, m.rink)
+	if err != nil {
+		return err
+	}
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *Match) update(db *sql.DB) error {
+	return errors.New("not implemented");
+}
