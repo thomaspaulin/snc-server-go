9d56cc79ab0c2f1ddd1178eb9cb53295a2d3141b
Thomas Paulin <thomas@biomatters.com>
2017-10-31 15:58:16 +1300

    - Bring functions closer to their corresponding structs

--- match.go
+++ match.go
@@ -19,8 +19,9 @@ const (
 	ShortHandedGoal = 	"SH"
 )
 
-// Models
-
+//-----------------------------------------------//
+// Match
+//-----------------------------------------------//
 type Match struct {
 	ID			uint32		`json:"id"`
 	// Datetime of the match start in UTC
@@ -33,52 +34,6 @@ type Match struct {
 	Rink		string		`json:"rink"`
 }
 
-// TODO: unify the match and match summary classes? In a way they are the same thing
-type MatchSummary struct {
-	MatchID		uint32		`json:"matchId"`
-	// Datetime of the match start in UTC
-	Start		time.Time	`json:"start"`
-	Away		string		`json:"away"`
-	Home		string		`json:"home"`
-	AwayScore	int			`json:"awayScore"`
-	HomeScore	int			`json:"homeScore"`
-	Rink		string		`json:"rink"`
-	Goals		[]*Goal		`json:"goals"`
-	Penalties	[]*Penalty	`json:"penalties"`
-	// todo:
-	//  - shots (per team, per period)
-	//  - power plays (per team, successful and total)
-	//  - players and goalies indexed by team
-}
-
-type Goal struct {
-	ID			uint32		`json:"id"`
-	GoalType	string		`json:"goalType"`
-	// ID of the team that scored
-	Team		string		`json:"team"`
-	Period		uint		`json:"period"`
-	// Seconds left in the period when the goal was scored
-	Time		uint		`json:"time"`
-	// ID of the scoring player
-	Scorer		string		`json:"scorer"`
-	Assists		[]string	`json:"assists"`
-}
-
-type Penalty struct {
-	ID			uint32		`json:"id"`
-	Team		string		`json:"team"`
-	Period		uint		`json:"period"`
-	// Seconds left in the period when the penalty was incurred
-	Time		uint		`json:"time"`
-	// Name of the penalty
-	Offense		string		`json:"offense"`
-	// ID of the offender
-	Offender	string		`json:"offender"`
-	// Penalty Infraction Minutes
-	PIM			uint		`json:"pim"`
-}
-
-// Database logic
 func (m *Match) Save() (id uint32, err error) {
 	id = 0
 	if m.ID > 0 {
@@ -142,4 +97,58 @@ func FetchMatch(id uint32) (*Match, error) {
 
 func (m *Match) Update() (id uint32, err error) {
 	return 0, errors.New("not implemented");
+}
+
+// TODO: unify the match and match summary classes? In a way they are the same thing
+//-----------------------------------------------//
+// Match Summary
+//-----------------------------------------------//
+type MatchSummary struct {
+	MatchID		uint32		`json:"matchId"`
+	// Datetime of the match start in UTC
+	Start		time.Time	`json:"start"`
+	Away		string		`json:"away"`
+	Home		string		`json:"home"`
+	AwayScore	int			`json:"awayScore"`
+	HomeScore	int			`json:"homeScore"`
+	Rink		string		`json:"rink"`
+	Goals		[]*Goal		`json:"goals"`
+	Penalties	[]*Penalty	`json:"penalties"`
+	// todo:
+	//  - shots (per team, per period)
+	//  - power plays (per team, successful and total)
+	//  - players and goalies indexed by team
+}
+
+//-----------------------------------------------//
+// Goal
+//-----------------------------------------------//
+type Goal struct {
+	ID			uint32		`json:"id"`
+	GoalType	string		`json:"goalType"`
+	// ID of the team that scored
+	Team		string		`json:"team"`
+	Period		uint		`json:"period"`
+	// Seconds left in the period when the goal was scored
+	Time		uint		`json:"time"`
+	// ID of the scoring player
+	Scorer		string		`json:"scorer"`
+	Assists		[]string	`json:"assists"`
+}
+
+//-----------------------------------------------//
+// Penalty
+//-----------------------------------------------//
+type Penalty struct {
+	ID			uint32		`json:"id"`
+	Team		string		`json:"team"`
+	Period		uint		`json:"period"`
+	// Seconds left in the period when the penalty was incurred
+	Time		uint		`json:"time"`
+	// Name of the penalty
+	Offense		string		`json:"offense"`
+	// ID of the offender
+	Offender	string		`json:"offender"`
+	// Penalty Infraction Minutes
+	PIM			uint		`json:"pim"`
 }
