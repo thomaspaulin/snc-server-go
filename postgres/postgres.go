c3a6f007d09935fe8a7dfeb4f2d3612d3dbcfe0e
Thomas Paulin <thomas@biomatters.com>
2017-11-04 15:46:46 +1300

    - Begin moving matches to new paradigm

--- postgres/postgres.go
+++ postgres/postgres.go
@@ -88,6 +88,7 @@ func (ds *DivisionService) UpdateDivision(d *snc.Division) error {
 }
 
 func (ds *DivisionService) DeleteDivision(id int) error {
+	// todo deleting a division should delete all the teams in the division because teams are tied to divisions
 	deleted := false
 	err := ds.DB.QueryRow(`
 		UPDATE divisions SET
@@ -100,6 +101,113 @@ func (ds *DivisionService) DeleteDivision(id int) error {
 	}
 	return err
 }
+
+
+//--------------------------------------------------------------------------------------------------------------------//
+// Matches
+//--------------------------------------------------------------------------------------------------------------------//
+// MatchServices represents the PostgreSQL implementation of snc.MatchService
+type MatchService struct {
+	DB *sql.DB
+}
+
+// todo match divisions
+
+//func (ms *MatchService) CreateMatch(m *snc.Match) error {
+//	id := 0
+//	err := ms.DB.QueryRow(`
+//	INSERT INTO matches
+//	 	(start, season, status, away_id, home_id, away_score, home_score, rink_id)
+//	VALUES
+//		($1, $2, $3, $4, $5, 0, 0, $6)
+//	RETURNING match_id`, m.Start, m.Season, m.Status, awayID, homeID, m.AwayScore, m.HomeScore, rinkID).Scan(&id)
+//	if err != nil {
+//		log.Println(err.Error())
+//	}
+//	return err
+//}
+//
+//func (ms *MatchService) Match(id int) (*snc.Match, error) {
+//	m := snc.Match{}
+//	err := ms.DB.QueryRow(`
+//	SELECT
+//		m.match_id,
+//		m.start,
+//		m.season,
+//		m.status,
+//		away.name,
+//		home.name,
+//		m.away_score,
+//		m.home_score,
+//		rink.name
+//	FROM (matches AS m
+//		INNER JOIN teams AS home
+//			ON home.team_id = m.home_id
+//		INNER JOIN teams AS away
+//			ON away.team_id = m.away_id
+//		INNER JOIN rinks AS rink
+//			ON rink.rink_id = m.rink_id)
+//	WHERE m.match_id = $1 AND m.deleted IS FALSE`, id).
+//		Scan(&m.ID, &m.Start, &m.Season, &m.Status, &m.Away, &m.Home, &m.AwayScore, &m.HomeScore, &m.Rink)
+//	if err == sql.ErrNoRows {
+//		return nil, nil
+//	} else if err != nil {
+//		return nil, err
+//	}
+//	return &m, nil
+//}
+//
+//func (ms *MatchService) Matches() ([]*snc.Match, error) {
+//	rows, err := ms.DB.Query(`
+//	SELECT
+//		m.match_id,
+//		m.start,
+//		m.season,
+//		m.status,
+//		away.name,
+//		home.name,
+//		m.away_score,
+//		m.home_score,
+//		rink.name
+//	FROM (matches AS m
+//		INNER JOIN teams AS home
+//			ON home.team_id = m.home_id
+//		INNER JOIN teams AS away
+//			ON away.team_id = m.away_id
+//		INNER JOIN rinks AS rink
+//			ON rink.rink_id = m.rink_id)
+//	WHERE m.deleted IS FALSE
+//	ORDER BY m.start DESC`)
+//	if err != nil {
+//		return nil, err
+//	}
+//	defer rows.Close()
+//
+//	matches := make([]*snc.Match, 0)
+//	for rows.Next() {
+//		m := snc.Match{}
+//		err := rows.Scan(&m.ID, &m.Start, &m.Season, &m.Status, &m.Away, &m.Home, &m.AwayScore, &m.HomeScore, &m.Rink)
+//		// err here is the row.Scan(...) error. It shadows the err from outside the loop, and does not overwrite
+//		if err != nil {
+//			// probably the schema is wrong or the row is bad and so the database needs inspecting
+//			// later on this might want to be changed to pass through and list the IDs of the bad rows
+//			return nil, err
+//		}
+//		matches = append(matches, &m)
+//	}
+//	if err = rows.Err(); err != nil {
+//		return nil, err
+//	}
+//	return matches, nil
+//}
+//
+//func (ms *MatchService) UpdateMatch(m *snc.Match) error {
+//
+//}
+//
+//func (ms *MatchService) DeleteMatch(id int) error {
+//
+//}
 
 
 //--------------------------------------------------------------------------------------------------------------------//
@@ -304,6 +412,7 @@ func (ts *TeamService) UpdateTeam(t *snc.Team) error {
 }
 
 func (ts *TeamService) DeleteTeam(id int) error {
+	// Deleting a team should NOT delete a division because divisions don't have dependencies on teams
 	deleted := false
 	err := ts.DB.QueryRow(`
 		UPDATE teams SET
--- snc/match.go
+++ snc/match.go
@@ -1,195 +1,99 @@
 package snc
-//
-//import (
-//	"time"
-//	"database/sql"
-//	"errors"
-//	"log"
-//)
-//
-//// todo handle the errors properly
-//const (
-//	PracticeGame 	= 	"PR"
-//	RegularGame 	= 	"RS"
-//	PlayoffGame 	= 	"PO"
-//
-//	RegularGoal 	= 	"RG"
-//	PowerPlayGoal 	= 	"PP"
-//	ShortHandedGoal = 	"SH"
-//
-//	Upcoming 		=	"Upcoming"
-//	Underway		=	"Underway"
-//	Over			=	"Over"
-//)
-//
-////-----------------------------------------------//
-//// Match
-////-----------------------------------------------//
-//type Match struct {
-//	ID			uint32		`json:"id"`
-//	// Datetime of the match start in UTC
-//	Start 		time.Time	`json:"start"`
-//	Season		int			`json:"season"`
-//	Status		string		`json:"status"`
-//	Division	string		`json:"division"`
-//	Away 		string		`json:"away"`
-//	Home 		string		`json:"home"`
-//	AwayScore	uint32		`json:"awayScore"`
-//	HomeScore	uint32		`json:"homeScore"`
-//	Rink		string		`json:"rink"`
-//}
-//// TODO UPDATE THE DATABASE SCHEMA SO THAT MATCHES HAVE A DIVISION_ID COLUMN
-//
-//
-//func (m *Match) Create(DB *sql.DB) (id uint32, err error) {
-//	// TODO: Save the teams and rinks first so we get the IDs and rink ID
-//	awayID, err := teamID(m.Away, m.Division)
-//	if err != nil {
-//		log.Println("I was unable to find the away team so I tried saving but that failed too. I don't know how to handle this case.")
-//		return 0, err
-//	}
-//	homeID, err := teamID(m.Home, m.Division)
-//	if err != nil {
-//		log.Println("I was unable to find the home team so I tried saving but that failed too. I don't know how to handle this case.")
-//		return 0, err
-//	}
-//
-//	rinkID, err := rinkID(m.Rink)
-//	if err != nil {
-//		return 0, err
-//	}
-//	// todo fix this
-//
-//	DB.QueryRow(`
-//	INSERT INTO matches
-//	 	(start, season, status, away_id, home_id, away_score, home_score, rink_id)
-//	VALUES
-//		($1, $2, $3, $4, $5, 0, 0, $6)
-//	RETURNING match_id`, m.Start, m.Season, m.Status, awayID, homeID, m.AwayScore, m.HomeScore, rinkID).Scan(&id)
-//	if err != nil {
-//		log.Println(err.Error())
-//	}
-//	return id, nil
-//}
-//
-//func FetchMatches(DB *sql.DB) ([]*Match, error) {
-//	rows, err := DB.Query(`
-//	SELECT
-//		m.match_id,
-//		m.start,
-//		m.season,
-//		m.status,
-//		away.name,
-//		home.name,
-//		m.away_score,
-//		m.home_score,
-//		rink.name
-//	FROM (matches AS m INNER JOIN teams AS home ON home.team_id = m.home_id
-//	INNER JOIN teams AS away ON away.team_id = m.away_id
-//	INNER JOIN rinks AS rink ON rink.rink_id = m.rink_id)
-//	ORDER BY m.start DESC`)
-//	if err != nil {
-//		return nil, err
-//	}
-//	defer rows.Close()
-//
-//	matches := make([]*Match, 0)
-//	for rows.Next() {
-//		m := Match{}
-//		err := rows.Scan(&m.ID, &m.Start, &m.Season, &m.Status, &m.Away, &m.Home, &m.AwayScore, &m.HomeScore, &m.Rink)
-//		// err here is the row.Scan(...) error. It shadows the err from outside the loop, and does not overwrite
-//		if err != nil {
-//			// probably the schema is wrong or the row is bad and so the database needs inspecting
-//			// later on this might want to be changed to pass through and list the IDs of the bad rows
-//			return nil, err
-//		}
-//		matches = append(matches, &m)
-//	}
-//	if err = rows.Err(); err != nil {
-//		return nil, err
-//	}
-//	return matches, nil
-//}
-//
-//func FetchMatch(DB *sql.DB, id uint32) (*Match, error) {
-//	m := Match{ID: id}
-//	err := DB.QueryRow(`
-//	SELECT
-//		m.match_id,
-//		m.start,
-//		m.season,
-//		m.status,
-//		away.name,
-//		home.name,
-//		m.away_score,
-//		m.home_score,
-//		rink.name
-//	FROM (matches AS m INNER JOIN teams AS home ON home.team_id = m.home_id
-//	INNER JOIN teams AS away ON away.team_id = m.away_id
-//	INNER JOIN rinks AS rink ON rink.rink_id = m.rink_id)
-//	WHERE match_id = $1`, id).Scan(&m.ID, &m.Start, &m.Season, &m.Status, &m.Away, &m.Home, &m.AwayScore, &m.HomeScore, &m.Rink)
-//	if err == sql.ErrNoRows {
-//		return &Match{}, nil
-//	} else if err != nil {
-//		return nil, err
-//	}
-//	return &m, nil
-//}
-//
-//func (m *Match) Update(DB *sql.DB) (id uint32, err error) {
-//	return 0, errors.New("not implemented");
-//}
-//
-//// TODO: unify the match and match summary classes? In a way they are the same thing
-////-----------------------------------------------//
-//// Match Summary
-////-----------------------------------------------//
-//type MatchSummary struct {
-//	MatchID		uint32		`json:"matchId"`
-//	// Datetime of the match start in UTC
-//	Start		time.Time	`json:"start"`
-//	Away		string		`json:"away"`
-//	Home		string		`json:"home"`
-//	AwayScore	int			`json:"awayScore"`
-//	HomeScore	int			`json:"homeScore"`
-//	Rink		string		`json:"rink"`
-//	Goals		[]*Goal		`json:"goals"`
-//	Penalties	[]*Penalty	`json:"penalties"`
-//	// todo:
-//	//  - shots (per team, per period)
-//	//  - power plays (per team, successful and total)
-//	//  - players and goalies indexed by team
-//}
-//
-////-----------------------------------------------//
-//// Goal
-////-----------------------------------------------//
-//type Goal struct {
-//	ID			uint32		`json:"id"`
-//	GoalType	string		`json:"goalType"`
-//	// ID of the team that scored
-//	Team		string		`json:"team"`
-//	Period		uint		`json:"period"`
-//	// Seconds left in the period when the goal was scored
-//	Time		uint		`json:"time"`
-//	// ID of the scoring player
-//	Scorer		string		`json:"scorer"`
-//	Assists		[]string	`json:"assists"`
-//}
-//
-////-----------------------------------------------//
-//// Penalty
-////-----------------------------------------------//
-//type Penalty struct {
-//	ID			uint32		`json:"id"`
-//	Team		string		`json:"team"`
-//	Period		uint		`json:"period"`
-//	// Seconds left in the period when the penalty was incurred
-//	Time		uint		`json:"time"`
-//	// Name of the penalty
-//	Offense		string		`json:"offense"`
-//	// ID of the offender
-//	Offender	string		`json:"offender"`
-//	// Penalty Infraction Minutes
-//	PIM			uint		`json:"pim"`
-//}
+
+import (
+	"time"
+)
+
+// todo handle the errors properly
+const (
+	PracticeGame 	= 	"PR"
+	RegularGame 	= 	"RS"
+	PlayoffGame 	= 	"PO"
+
+	RegularGoal 	= 	"RG"
+	PowerPlayGoal 	= 	"PP"
+	ShortHandedGoal = 	"SH"
+
+	Upcoming 		=	"Upcoming"
+	Underway		=	"Underway"
+	Over			=	"Over"
+)
+
+//-----------------------------------------------//
+// Match
+//-----------------------------------------------//
+type Match struct {
+	ID			uint32		`json:"id"`
+	// Datetime of the match start in UTC
+	Start 		time.Time	`json:"start"`
+	Season		int			`json:"season"`
+	Status		string		`json:"status"`
+	Division	string		`json:"division"`
+	Away 		string		`json:"away"`
+	Home 		string		`json:"home"`
+	AwayScore	uint32		`json:"awayScore"`
+	HomeScore	uint32		`json:"homeScore"`
+	Rink		string		`json:"rink"`
+}
+
+type MatchService interface {
+	CreateMatch(m *Match) error
+	Match(id int) error
+	Matches() ([]*Match, error)
+	UpdateMatch(m *Match) error
+	DeleteMatch(id int) error
+}
+
+// TODO: unify the match and match summary classes? In a way they are the same thing
+//-----------------------------------------------//
+// Match Summary
+//-----------------------------------------------//
+type MatchSummary struct {
+	MatchID		uint32		`json:"matchId"`
+	// Datetime of the match start in UTC
+	Start		time.Time	`json:"start"`
+	Away		string		`json:"away"`
+	Home		string		`json:"home"`
+	AwayScore	int			`json:"awayScore"`
+	HomeScore	int			`json:"homeScore"`
+	Rink		string		`json:"rink"`
+	Goals		[]*Goal		`json:"goals"`
+	Penalties	[]*Penalty	`json:"penalties"`
+	// todo:
+	//  - shots (per team, per period)
+	//  - power plays (per team, successful and total)
+	//  - players and goalies indexed by team
+}
+
+//-----------------------------------------------//
+// Goal
+//-----------------------------------------------//
+type Goal struct {
+	ID			uint32		`json:"id"`
+	GoalType	string		`json:"goalType"`
+	// ID of the team that scored
+	Team		string		`json:"team"`
+	Period		uint		`json:"period"`
+	// Seconds left in the period when the goal was scored
+	Time		uint		`json:"time"`
+	// ID of the scoring player
+	Scorer		string		`json:"scorer"`
+	Assists		[]string	`json:"assists"`
+}
+
+//-----------------------------------------------//
+// Penalty
+//-----------------------------------------------//
+type Penalty struct {
+	ID			uint32		`json:"id"`
+	Team		string		`json:"team"`
+	Period		uint		`json:"period"`
+	// Seconds left in the period when the penalty was incurred
+	Time		uint		`json:"time"`
+	// Name of the penalty
+	Offense		string		`json:"offense"`
+	// ID of the offender
+	Offender	string		`json:"offender"`
+	// Penalty Infraction Minutes
+	PIM			uint		`json:"pim"`
+}
