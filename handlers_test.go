d4713900bd87781766c67709884159130d6b8dae
Thomas Paulin <thomas@biomatters.com>
2017-11-06 18:17:36 +1300

    - Add another test

--- handlers_test.go
+++ handlers_test.go
@@ -54,3 +54,52 @@ func TestGetTeams(t *testing.T) {
 	// todo is this looking at memory address or contents?
 	assert.Equal(t, teams, actual)
 }
+
+func TestGetSpecificRink(t *testing.T) {
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/v1/teams/1", nil)
+	team := snc.Team{ID: 1, Division: "C", Name: "Bears"}
+
+	s := Services{}
+	ts := mock.TeamService{}
+	ts.TeamFn = func(id int) (*snc.Team, error) {
+		assert.Equal(t, id, 1);
+		return &team, nil
+	}
+
+	s.TeamService = &ts
+
+	// this means the entire API has to be created again for each test :(
+	r := APIEngine(s)
+	r.ServeHTTP(w, req)
+
+	assert.True(t, ts.TeamInvoked)
+	assert.Equal(t, 200, w.Code)
+
+	actual := snc.Team{}
+	json.NewDecoder(w.Body).Decode(&actual)
+	assert.Equal(t, team, actual)
+}
+
+// todo uncomment when team creation is supported
+//func TestCreateTeam(t *testing.T) {
+//	team := snc.Team{Division: "C", Name: "Lions"}
+//
+//	w := httptest.NewRecorder()
+//	j, _ := json.Marshal(team)
+//	req := httptest.NewRequest("POST", "/v1/teams", bytes.NewBuffer(j))
+//
+//	s := Services{}
+//	ts := mock.TeamService{}
+//	ts.CreateFn = func(t *snc.Team) error {
+//		return nil
+//	}
+//
+//	s.TeamService = &ts
+//	r := APIEngine(s)
+//	r.ServeHTTP(w, req)
+//
+//	assert.True(t, ts.CreateInvoked)
+//	assert.Equal(t, 200, w.Code)
+//}
+
