f71d5c4fa5bec7c24121c6d5a3fd275fbe41660a
Thomas Paulin <thomas@biomatters.com>
2017-11-13 17:17:06 +1300

    - Remove services

--- snc/match.go
+++ snc/match.go
@@ -2,6 +2,7 @@ package snc
 
 import (
 	"time"
+	"database/sql"
 )
 
 // todo handle the errors properly
@@ -28,20 +29,124 @@ type Match struct {
 	Start 		time.Time	`json:"start"`
 	Season		int			`json:"season"`
 	Status		string		`json:"status"`
-	Division	string		`json:"division"`
-	Away 		string		`json:"away"`
-	Home 		string		`json:"home"`
+	Division	Division	`json:"division"`
+	Away 		Team		`json:"away"`
+	Home 		Team		`json:"home"`
 	AwayScore	uint		`json:"awayScore"`
 	HomeScore	uint		`json:"homeScore"`
-	Rink		string		`json:"rink"`
+	Rink		Rink		`json:"rink"`
 }
 
-type MatchService interface {
-	CreateMatch(m *Match) error
-	Match(id int) (*Match, error)
-	Matches() ([]*Match, error)
-	UpdateMatch(m *Match) error
-	DeleteMatch(id int) error
+func CreateMatch(m Match) error {
+	//id := 0
+	//err := ms.DB.QueryRow(`
+	//INSERT INTO matches
+	// 	(start, season, status, away_id, home_id, away_score, home_score, rink_id)
+	//VALUES
+	//	($1, $2, $3, $4, $5, 0, 0, $6)
+	//RETURNING match_id`, m.Start, m.Season, m.Status, awayID, homeID, m.AwayScore, m.HomeScore, rinkID).Scan(&id)
+	//if err != nil {
+	//	log.Println(err.Error())
+	//}
+	//return err
+	return nil
+}
+
+func FetchMatch(id int, DB *sql.DB) (Match, error) {
+	m := Match{}
+	var divId, awayId, homeId, rinkId uint
+	err := DB.QueryRow(`
+	SELECT
+		m.match_id,
+		m.start,
+		m.season,
+		m.status,
+		away.name,
+		home.name,
+		m.away_score,
+		m.home_score,
+		rink.name
+	FROM (matches AS m
+		INNER JOIN teams AS home
+			ON home.team_id = m.home_id
+		INNER JOIN teams AS away
+			ON away.team_id = m.away_id
+		INNER JOIN rinks AS rink
+			ON rink.rink_id = m.rink_id)
+	WHERE m.match_id = $1 AND m.deleted IS FALSE`, id).
+		Scan(&m.ID, &m.Start, &m.Season, divId, &m.Status, awayId, homeId, &m.AwayScore, &m.HomeScore, rinkId)
+	div, _ := FetchDivision(divId, DB)
+	m.Division = div
+	away, _ := FetchTeam(awayId, DB)
+	m.Away = away
+	home, _ := FetchTeam(homeId, DB)
+	m.Home = home
+	rink, _ := FetchRink(rinkId, DB)
+	m.Rink = rink
+	if err == sql.ErrNoRows {
+		return m, nil
+	} else if err != nil {
+		return Match{}, err
+	}
+	return m, nil
+}
+
+func FetchMatches(DB *sql.DB) ([]Match, error) {
+	rows, err := DB.Query(`
+	SELECT
+		m.match_id,
+		m.start,
+		m.season,
+		m.division_id,
+		m.status,
+		m.away_id,
+		m.home_id,
+		m.away_score,
+		m.home_score,
+		m.rink_id
+	FROM matches AS m
+	WHERE m.deleted IS FALSE
+	ORDER BY m.start DESC`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	matches := make([]Match, 0)
+	var divId, awayId, homeId, rinkId uint
+	for rows.Next() {
+		m := Match{}
+		err := rows.Scan(&m.ID, &m.Start, &m.Season, divId, &m.Status, awayId, homeId, &m.AwayScore, &m.HomeScore, rinkId)
+		// err here is the row.Scan(...) error. It shadows the err from outside the loop, and does not overwrite
+		if err != nil {
+			// probably the schema is wrong or the row is bad and so the database needs inspecting
+			// later on this might want to be changed to pass through and list the IDs of the bad rows
+			return nil, err
+		}
+		div, _ := FetchDivision(divId, DB)
+		m.Division = div
+		away, _ := FetchTeam(awayId, DB)
+		m.Away = away
+		home, _ := FetchTeam(homeId, DB)
+		m.Home = home
+		rink, _ := FetchRink(rinkId, DB)
+		m.Rink = rink
+		matches = append(matches, m)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return matches, nil
+}
+
+func UpdateMatch(m Match, DB *sql.DB) error {
+	// todo
+	return nil
+}
+
+func DeleteMatch(id int, DB *sql.DB) error {
+	// todo
+	return nil
 }
 
 //-----------------------------------------------//
--- snc/team.go
+++ snc/team.go
@@ -2,6 +2,9 @@ package snc
 
 import (
 	"errors"
+	"database/sql"
+	"log"
+	"strings"
 )
 
 //--------------------------------------------------------------------------------------------------------------------//
@@ -12,19 +15,135 @@ type Team struct {
 	Name		string	`json:"name"`
 	Division	string	`json:"division"`
 	LogoURL		string	`json:"logoURL"`
-}
-
-// todo use copy rather than pointer
-type TeamService interface {
-	CreateTeam(t *Team) error
-	Team(id int) (*Team, error)
-	TeamCalled(name string) (*Team, error)
-	Teams() ([]*Team, error)
-	UpdateTeam(t *Team) error
-	DeleteTeam(id int) error
+	Players		[]Player`json:"players"`
 }
 
 var ErrMultipleTeams = errors.New("snc: expected only one team but got multiple")
+
+func CreateTeam(t Team, DB *sql.DB) error {
+	id := 0
+	// TODO save the division ID rather than just the 0. Be it through a query or having the division as a field on the team struct
+	err := DB.QueryRow(`
+	INSERT INTO teams
+		(name, division_id, logo_url)
+	VALUES
+  		($1, $2, $3)
+	RETURNING team_id`, strings.ToLower(t.Name), 0, t.LogoURL).Scan(&id)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return err
+}
+
+func FetchTeam(id uint, DB *sql.DB) (Team, error) {
+	t := Team{ID: id}
+	err := DB.QueryRow(`
+	SELECT
+  		teams.name     AS  team_name,
+  		divisions.name AS  div_name,
+  		teams.logo_url
+	FROM (teams
+		JOIN divisions ON teams.division_id = divisions.division_id
+	 )
+	 WHERE teams.team_id = $1 AND teams.deleted IS FALSE`, id).Scan(&t.Name, &t.Division, &t.LogoURL)
+	if err == sql.ErrNoRows {
+		return t, nil
+	} else if err != nil {
+		log.Println(err.Error())
+		return Team{Name: "Unknown"}, err
+	}
+	return t, nil
+}
+
+func FetchTeams(DB *sql.DB) ([]Team, error) {
+	// TODO create a new division if there isn't one?
+	rows, err := DB.Query(`
+	SELECT
+  		teams.team_id  AS team_id,
+  		teams.name     AS team_name,
+  		divisions.name AS div_name,
+  		teams.logo_url
+	FROM (teams
+		JOIN divisions
+      	ON teams.division_id = divisions.division_id
+	)
+	WHERE teams.deleted IS FALSE
+    ORDER BY team_id`)
+	if err != nil {
+		log.Println(err.Error())
+		// Connection or statement error
+		return nil, err
+	}
+	defer rows.Close()
+
+	teams := []Team{}
+	for rows.Next() {
+		t := Team{}
+		err := rows.Scan(&t.ID, &t.Name, &t.Division, &t.LogoURL)
+		if err != nil {
+			// Row parsing error. Pass over it but log it too
+			log.Println(err.Error())
+		}
+		teams = append(teams, t)
+	}
+	err = rows.Err()
+	if err != nil {
+		// Errors within rows
+		log.Println(err.Error())
+		return nil, err
+	}
+	rows.Close()
+	return teams, nil
+}
+
+func TeamCalled(name string, DB *sql.DB) (Team, error) {
+	t := Team{Name: name}
+	err := DB.QueryRow(`
+	SELECT
+  		teams.team_id  AS  team_id,
+  		divisions.name AS  div_name,
+  		teams.logo_url
+	FROM (teams
+		JOIN divisions ON teams.division_id = divisions.division_id
+	 )
+	 WHERE teams.name = $1 AND teams.deleted IS FALSE`, strings.ToLower(name)).Scan(&t.ID, &t.Division, &t.LogoURL)
+	if err == sql.ErrNoRows {
+		return t, nil
+	} else if err != nil {
+		log.Println(err.Error())
+		return Team{}, err
+	}
+	return t, nil
+}
+
+func UpdateTeam(t *Team, DB *sql.DB) error {
+	id := 0
+	err := DB.QueryRow(`
+		UPDATE teams SET
+			name = $1
+		WHERE
+			team_id = $2 AND deleted IS FALSE
+		RETURNING team_id`, strings.ToLower(t.Name), t.ID).Scan(&id)
+	if err != nil {
+		log.Println(err.Error());
+	}
+	return err
+}
+
+func DeleteTeam(id int, DB *sql.DB) error {
+	// Deleting a team should NOT delete a division because divisions don't have dependencies on teams
+	deleted := false
+	err := DB.QueryRow(`
+		UPDATE teams SET
+			deleted = TRUE
+		WHERE
+			team_id = $1
+		RETURNING deleted`, id).Scan(&deleted)
+	if err != nil {
+		log.Println(err.Error());
+	}
+	return err
+}
 
 //--------------------------------------------------------------------------------------------------------------------//
 // Division
@@ -34,12 +153,91 @@ type Division struct {
 	Name		string	`json:"name"`
 }
 
-type DivisionService interface {
-	CreateDivision(d *Division) error
-	Division(id int) (*Division, error)
-	Divisions() ([]*Division, error)
-	UpdateDivision(d *Division) error
-	DeleteDivision(id int) error
+func CreateDivision(d Division, DB *sql.DB) error {
+	// the returned ID is not being used for the time being
+	id := 0
+	err := DB.QueryRow(`
+		INSERT INTO divisions
+			(name)
+		VALUES
+			($1)
+		RETURNING division_id`, d.Name).Scan(&id)
+	return err
+}
+
+func FetchDivision(id uint, DB *sql.DB) (Division, error) {
+	d := Division{ID: id}
+	err := DB.QueryRow(`
+	SELECT
+		division_id, name
+	FROM divisions
+	WHERE division_id = $1 AND deleted IS FALSE`, id).Scan(&d.ID, &d.Name)
+	if err == sql.ErrNoRows {
+		return d, nil
+	} else if err != nil {
+		return Division{Name: "Unknown"}, err
+	}
+	return d, nil
+}
+
+func FetchDivisions(DB *sql.DB) ([]Division, error) {
+	rows, err := DB.Query(`
+	SELECT
+  		division_id, name
+	FROM divisions
+	WHERE deleted IS FALSE`)
+	if err != nil {
+		// Connection or statement error
+		return nil, err
+	}
+	defer rows.Close()
+
+	divs := []Division{}
+	for rows.Next() {
+		d := Division{}
+		err := rows.Scan(&d.ID, &d.Name)
+		// skip the division if there's an error but log it anyway
+		if err != nil {
+			log.Printf("postgres: error encountered when scanning a row. Row will be logged but here is the error: %s\n", err.Error())
+		}
+		divs = append(divs, d)
+	}
+	err = rows.Err()
+	if err != nil {
+		// Errors within rows
+		return nil, err
+	}
+	rows.Close()
+	return divs, nil
+}
+
+func UpdateDivision(d Division, DB *sql.DB) error {
+	id := 0
+	err := DB.QueryRow(`
+		UPDATE divisions SET
+			name = $1
+		WHERE
+			division_id = $2 AND deleted IS FALSE
+		RETURNING division_id`, d.Name, d.ID).Scan(&id)
+	if err != nil {
+		log.Println(err.Error());
+	}
+	return err
+}
+
+func DeleteDivision(id int, DB *sql.DB) error {
+	// todo deleting a division should delete all the teams in the division because teams are tied to divisions
+	deleted := false
+	err := DB.QueryRow(`
+		UPDATE divisions SET
+			deleted = TRUE
+		WHERE
+			division_id = $1
+		RETURNING deleted`).Scan(&deleted)
+	if err != nil {
+		log.Println(err.Error());
+	}
+	return err
 }
 
 //--------------------------------------------------------------------------------------------------------------------//
@@ -49,6 +247,6 @@ type Player struct {
 	ID			uint		`json:"id"`
 	Number		uint		`json:"number"`
 	Name		string		`json:"name"`
-	Teams		[]string	`json:"teams"`
+	//Teams		[]string	`json:"teams"`
 	Position	string		`json:"position"`
 }
